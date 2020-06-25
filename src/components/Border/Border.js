464571269a72b09404e4de465e2ee54713f9a082
Carlton <Carlton.l@outlook.com>
2020-06-24 19:46:14 -0700

    Update Border.js

--- src/components/Border/Border.js
+++ src/components/Border/Border.js
@@ -4,16 +4,7 @@ import PropTypes from 'prop-types';
 const Border = (props) => {
     const { set } = props;
 
-    switch (set) {
-        case 'left':
-            return <div className="greek-border left"></div>;
-        case 'right':
-            return <div className="greek-border right"></div>;
-        case 'center':
-            return <div></div>;
-        default:
-            return <div></div>;
-    }
+    return <div className={`greek-border ${set}`}></div>;
 };
 
 Border.propTypes = {
