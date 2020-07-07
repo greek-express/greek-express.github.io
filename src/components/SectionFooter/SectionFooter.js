15290b2106c3ad51e230a689296462a752e8d379
Carlton <Carlton.l@outlook.com>
2020-07-07 10:22:16 -0700

    Footer and topbar styling

--- src/components/SectionFooter/SectionFooter.js
+++ src/components/SectionFooter/SectionFooter.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
 const SectionFooter = () => {
-    return <div>greekexpress@gmail.com</div>;
+    return (
+        <section className="section-footer">
+            <div className="footer-container">
+                <div className="text-content" style={{ color: '#dcdcdc' }}>
+                    <a
+                        href="mailto:business.greek.express@gmail.com"
+                        className="email"
+                    >
+                        business.greek.express@gmail.com
+                    </a>
+                </div>
+                <div className="text-content" style={{ color: '#dcdcdc' }}>
+                    <a href="tel:+1-425-747-7434" className="phone">
+                        (425)-747-7434
+                    </a>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default SectionFooter;
